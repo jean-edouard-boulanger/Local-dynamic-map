04a2d9a83df22dbb4df57aeede1de99336a7abd4
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2014-12-12 17:24:34 +0100

    Creation structure classe CarAgent

---
+++ src/com/ldm/model/geometry/Position.java
@@ -0,0 +1,33 @@
+package com.ldm.model.geometry;
+
+public class Position {
+	private double x = 0.0;
+	private double y = 0.0;
+	
+	public Position(){}
+	
+	public Position(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {return x;}
+
+	public void setX(double x) {this.x = x;}
+
+	public double getY() {return y;}
+
+	public void setY(double y) {this.y = y;}
+	
+	public static Double evaluateSquareDistance(Position p1, Position p2){
+		if(p1 == null || p2 == null){return null;}
+		
+		return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+	}
+	
+	public static Double evaluateDistance(Position p1, Position p2){
+		Double d = evaluateSquareDistance(p1, p2);
+		if(d == null){return null;}
+		return Math.sqrt(d);
+	}
+}
--- src/com/ldm/sma/agent/CarAgent.java
+++ src/com/ldm/sma/agent/CarAgent.java
@@ -6,10 +6,16 @@ import javafx.embed.swing.JFXPanel;
 
 import javax.swing.SwingUtilities;
 
+import com.ldm.model.geometry.Position;
 import com.ldm.ui.CarUI;
 
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.domain.introspection.SentMessage;
 import jade.gui.GuiAgent;
 import jade.gui.GuiEvent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 public class CarAgent extends GuiAgent {
 
@@ -19,6 +25,11 @@ public class CarAgent extends GuiAgent {
 	public CarAgent(){
 		super();
 		propertyChangeCarAgent = new PropertyChangeSupport(this);
+	}
+	
+	public Position getCurrentPosition(){
+		// Temporaire, a modifier
+		return new Position(0.0, 0.0);
 	}
 	
 	@Override
@@ -35,14 +46,72 @@ public class CarAgent extends GuiAgent {
 						CarAgent.this.carUI = CarUI.startUI(CarAgent.this, propertyChangeCarAgent);
 					}
 				});
-
+				
 			}
 		});
+		
+		this.addBehaviour(new DistanceFilterBehaviour(this, 500));
+		
+		this.addBehaviour(new HandleMessagesBehaviour(this));
 	}
 
 	@Override
 	protected void onGuiEvent(GuiEvent arg0) {
 
+	}
+	
+	/**
+	 * Simulates the distance between vehicles
+	 * Drops a message whenever the distance between the sender car the and receiver is too wide (Greater than maximumDistance)
+	 */
+	public class DistanceFilterBehaviour extends Behaviour{
+		
+		private double maximumDistance = 0.;
+		
+		public DistanceFilterBehaviour(Agent agent, double maximumDistance){
+			super(agent);
+			this.maximumDistance = maximumDistance;
+		}
+		
+		@Override
+		public void action() {
+			ACLMessage message = this.myAgent.receive(AgentHelper.filterDistanceProtocol);
+			if(message != null){
+				message.setProtocol(null);
+				
+				Position sentAtPosition = AgentHelper.getSentAtPosition(message);
+				Position currentPosition = CarAgent.this.getCurrentPosition();
+				
+				if(sentAtPosition != null && maximumDistance * maximumDistance > Position.evaluateSquareDistance(currentPosition, sentAtPosition)){
+					this.myAgent.send(message);
+				}
+			}
+			else{
+				this.block();
+			}
+		}
+
+		@Override
+		public boolean done() {
+			return false;
+		}
+	}
+	
+	public class HandleMessagesBehaviour extends Behaviour{
+		
+		public HandleMessagesBehaviour(Agent agent){
+			super(agent);
+		}
+		
+		@Override
+		public void action() {
+			
+		}
+
+		@Override
+		public boolean done() {
+			return false;
+		}
 	}
 	
 }
