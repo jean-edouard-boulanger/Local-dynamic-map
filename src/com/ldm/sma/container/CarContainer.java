a8abf198c6bc28e5859e4e24b23479a0063b090f
Jean-Edouard BOULANGER <jean.edouard.boulanger@gmail.com>
2014-12-12 14:52:14 +0100

    Ajout de CarContainer (Lance un conteneur contenant un agent Car)

---
+++ src/com/ldm/sma/container/CarContainer.java
@@ -0,0 +1,31 @@
+package com.ldm.sma.container;
+
+import java.util.Random;
+
+import jade.core.AgentContainer;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+
+public class CarContainer {
+	
+	public static void main(String[] args){
+		Runtime runtime = Runtime.instance();
+		Profile profile = null;
+		try{
+			profile = new ProfileImpl("slave_container.property");
+			ContainerController container = runtime.createAgentContainer(profile);
+			
+			AgentController carAgent = container.createNewAgent("Voiture" + (new Random()).nextInt(), "com.ldm.sma.agent.Car", null);
+			
+			carAgent.start();
+		}
+		catch(Exception e){
+			System.err.println("Impossible de démarrer la voiture: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+}
